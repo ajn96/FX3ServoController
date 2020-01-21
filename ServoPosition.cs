120ce83ab6e621ac52d5395cb23a6352d09d3878
Alex <alex.nolan@analog.com>
2020-01-20 21:43:32 -0500

    Implemented servo position class

--- ServoPosition.cs
+++ ServoPosition.cs
@@ -4,7 +4,53 @@ using System.Text;
 
 namespace FX3ServoController
 {
+    /// <summary>
+    /// A servo angle and the rotation speed to reach that angle
+    /// </summary>
     class ServoPosition
     {
+        //Private members
+        private double m_targetAngle;
+        private double m_rotationSpeed;
+
+        /// <summary>
+        /// Contstructor. Sets default values.
+        /// </summary>
+        public ServoPosition()
+        {
+            m_targetAngle = 0;
+            m_rotationSpeed = 15;
+        }
+
+        /// <summary>
+        /// Get or set the position target angle (degrees). Cannot be more than 360 degrees.
+        /// </summary>
+        public double TargetAngle
+        {
+            get
+            {
+                return m_targetAngle;
+            }
+            set
+            {
+                m_targetAngle = value % 360;
+            }
+        }
+
+        /// <summary>
+        /// Get or set the rotation speed to reach the target angle (degrees / second)
+        /// </summary>
+        public double RotationSpeed
+        {
+            get
+            {
+                return m_rotationSpeed;
+            }
+            set
+            {
+                m_rotationSpeed = value;
+            }
+        }
+
     }
 }
