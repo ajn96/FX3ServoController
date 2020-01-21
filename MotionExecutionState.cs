79092c7ca72e7221cba9b35e2e22e0b376a5bf6a
Alex <alex.nolan@analog.com>
2020-01-20 23:43:04 -0500

    Added motion execution state class

---
+++ MotionExecutionState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FX3ServoController
+{
+    class MotionExecutionState
+    {
+        public ServoMotor Motor;
+        public int MotionProfileIndex;
+        public List<ServoPosition> MotionProfile;
+        public int CurrentIteration;
+        public int TotalIterations;
+        public double CurrentAngle;
+        public bool isRunning;
+        public long LastUpdateTime;
+
+        public MotionExecutionState()
+        {
+            MotionProfileIndex = 0;
+            CurrentIteration = 0;
+        }
+    }
+}
